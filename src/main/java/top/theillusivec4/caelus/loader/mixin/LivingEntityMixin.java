01157bf6325f89895a2adef8848d48bcf61e9b5f
TheIllusiveC4 <itwasmec4@gmail.com>
2020-07-25 17:48:41 -0700

    fix custom elytra flight check

--- src/main/java/top/theillusivec4/caelus/loader/mixin/LivingEntityMixin.java
+++ src/main/java/top/theillusivec4/caelus/loader/mixin/LivingEntityMixin.java
@@ -19,18 +19,27 @@
 
 package top.theillusivec4.caelus.loader.mixin;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.theillusivec4.caelus.loader.common.MixinHooks;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
+
+  public LivingEntityMixin(EntityType<?> type, World world) {
+    super(type, world);
+  }
 
   @SuppressWarnings("ConstantConditions")
-  @ModifyVariable(at = @At(value = "INVOKE", target = "net/minecraft/entity/LivingEntity.setFlag (IZ)V"), method = "initAi")
-  public boolean changeFlag(boolean flag) {
-    return flag || MixinHooks.canFly((LivingEntity) (Object) this);
+  @Inject(at = @At(value = "INVOKE", target = "net/minecraft/entity/LivingEntity.setFlag (IZ)V"), method = "initAi", cancellable = true)
+  public void setFlag(CallbackInfo cb) {
+    this.setFlag(7, MixinHooks.canFly((LivingEntity) (Object) this));
+    cb.cancel();
   }
 }
