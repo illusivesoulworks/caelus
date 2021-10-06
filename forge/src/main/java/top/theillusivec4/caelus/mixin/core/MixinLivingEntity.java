97429a4a17a8187d4d31265177244d48d14d571b
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-10-05 23:46:11 -0700

    fix fall flying, closes #26

--- forge/src/main/java/top/theillusivec4/caelus/mixin/core/MixinLivingEntity.java
+++ forge/src/main/java/top/theillusivec4/caelus/mixin/core/MixinLivingEntity.java
@@ -21,18 +21,25 @@
 
 package top.theillusivec4.caelus.mixin.core;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import top.theillusivec4.caelus.mixin.util.MixinHooks;
 
 @Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+public abstract class MixinLivingEntity extends Entity {
+
+  public MixinLivingEntity(EntityType<?> pEntityType, Level pLevel) {
+    super(pEntityType, pLevel);
+  }
 
   @SuppressWarnings("ConstantConditions")
   @ModifyArg(at = @At(value = "INVOKE", target = "net/minecraft/world/entity/LivingEntity.setSharedFlag(IZ)V"), method = "updateFallFlying")
   private boolean caelus$setFlag(boolean value) {
-    return MixinHooks.canFly((LivingEntity) (Object) this, value);
+    return MixinHooks.canFly((LivingEntity) (Object) this, this.getSharedFlag(7));
   }
 }
